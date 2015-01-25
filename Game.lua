be3f308bfc5f8d81e4627f9b8a1c1a2efbd8b5d1
ShadowDj101 <shadowdj101@yahoo.com>
2015-01-24 21:02:05 -0600

    Additions

--- Game.lua
+++ Game.lua
@@ -30,7 +30,7 @@ print("help - shows help")
 io.read()
 print("inspect - search area")
 io.read()
-print("exit____ - leaves area(use _ for space only for the exit cmd)")
+print("exit____ - leaves area")
 io.read()
 print("follow ____ - get on bridge/path or follow someone")
 io.read()
@@ -54,13 +54,14 @@ table.remove(inv, i)
 end
 end
 end
-function room(exit_cave_cmd, t)
+function room(t)
+room_exit = false
 input = ""
-while input ~= exit_cave_cmd do
+while not room_exit do
 input = get_input()
 valid = false
 for k,v in pairs(t) do
-if input == k then
+if input == string.gsub(k, "_", " ") then
 if type(v) == "string" then
 print(v)
 else
@@ -81,8 +82,13 @@ end
 -- Functions
 
 --In Cave
-room_cmd = {inspect = "You are in a cave. There is an exit.", exit_cave = "You leave the cave."}
-room("exit cave", room_cmd)
+room_cmd = {inspect = "You are in a cave. There is an exit.", 
+exit_cave = function()
+print("You leave the cave.")
+room_exit = true
+end
+}
+room(room_cmd)
 -- In Cave
 
 --Outside Of Cave
@@ -97,9 +103,11 @@ if fightinput == "attack" then
 print("You make a long, deep cut through its chest. It drops dead.")
 print("Added Goblin Armor to Inventory.")
 add_inv("Goblin Armor")
+room_exit = true
 elseif fightinput == "run away" then
 print("You coward. You run but get shot, causing you to stagger and lose the coin.You get away.")
 remove_inv("Un-Readable Coin")
+room_exit = true
 else
 print("You stand there and right before you die, you hear a loud voice say,'BOOM! HEADSHOT!'Game over.")
 io.read()
@@ -108,7 +116,7 @@ end
 end
 }
 
-room("follow", room_cmd2)
+room(room_cmd2)
 
 --Outside Of Cave
 
@@ -134,15 +142,16 @@ print("The gate is locked.")
 elseif have_key == true then
 print("You unlock the gate and leave. The key is destroyed. Good job. The key had a life to you know.")
 remove_inv("Thick Key")
+room_exit = true
 end
 end,
-grab_magic_wand = function()
-add_inv("Magic Wand")
+grab_magical_wand = function()
+add_inv("Magical Wand")
 print("You run outside, trying to combat the werecats, flailing your arms very girly with your female hands screaming the Harry Potter spell,'Peeus Hypeus!'")
 end
 }
 
-room("open", room_cmd3)
+room(room_cmd3)
 --At Gate
 
 --Player Wins
@@ -154,7 +163,7 @@ if input == "exit" then
 print("Goodbye!")
 io.read()
 os.exit()
-else input ~= "exit" then
+elseif input ~= "exit" then
 print("Please type 'exit' to leave.")
 end
 end
